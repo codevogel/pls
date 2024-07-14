d3be2f826a23ef3b9d55a7e4baaa0499feda8247
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-14 18:28:29 +0200

    group samples in valid and faulty

--- spec/query/query_command_in_file_spec.sh
+++ spec/query/query_command_in_file_spec.sh
@@ -22,33 +22,39 @@ End
 Describe 'query_command_in_file catches format error'
   
   top_level_error() {
-    echo "Format Error: 'samples/query/$1.yml' should have 'commands' as the (only) top-level node."
+    echo "Format Error: 'samples/faulty/$1.yml' should have 'commands' as the (only) top-level node."
     echo "It currently has $2 top-level nodes."
     echo "$3 of which are 'commands'."
   }
 
   alias_not_unique_error() {
-    echo "Format Error: 'samples/query/[commands][foo,foo].yml' has multiple commands with the alias 'foo'."
+    echo "Format Error: 'samples/faulty/[commands][foo,foo].yml' has multiple commands with the alias 'foo'."
     echo "It should have only one."
     echo "It currently has 2 occurances."
   }
 
   alias_has_no_command() {
-    echo "Format Error: 'samples/query/[commands][has no command].yml' has an alias 'has no command', but no command."
+    echo "Format Error: 'samples/faulty/[commands][has no command].yml' has an alias 'has no command', but no command."
     echo "Each alias should have exactly one command."
   }
 
+  local empty_file='[]'
+  local invalid_top_level_key='[notcommands][]'
+  local duplicate_top_level_key='[commands,commands][]'
+  local valid_and_invalid_top_level_key='[commands,notcommands][]'
+  local duplicate_alias='[commands][foo,foo]'
+  local no_command='[commands][has no command]'
   Parameters
-    '[]' "foo" "$(top_level_error "[]" 0 0)"
-    '[notcommands][]' "foo" "$(top_level_error "[notcommands][]" 1 0)"
-    '[commands,commands][]' "foo" "$(top_level_error "[commands,commands][]" 2 2)"
-    '[commands,notcommands][]' "foo" "$(top_level_error "[commands,notcommands][]" 2 1)"
-    '[commands][foo,foo]' "foo" "$(alias_not_unique_error)"
-    '[commands][has no command]' "has no command" "$(alias_has_no_command)" 
+    "$empty_file" "foo" "$(top_level_error "$empty_file" 0 0)"
+    "$invalid_top_level_key" "foo" "$(top_level_error "$invalid_top_level_key" 1 0)"
+    "$duplicate_top_level_key" "foo" "$(top_level_error "$duplicate_top_level_key" 2 2)"
+    "$valid_and_invalid_top_level_key" "foo" "$(top_level_error "$valid_and_invalid_top_level_key" 2 1)"
+    "$duplicate_alias" "foo" "$(alias_not_unique_error)"
+    "$no_command" "has no command" "$(alias_has_no_command)"
   End
 
   It "getting '$2' from $1"
-    When call query_command_in_file "$2" "samples/query/$1.yml" 
+    When call query_command_in_file "$2" "samples/faulty/$1.yml" 
     The status should be failure
     The stderr should eq "$3"
     The stdout should be blank
@@ -65,7 +71,7 @@ Describe 'query_command_in_file returns command'
   End
 
   It "getting '$2' from $1"
-    When call query_command_in_file "$2" "samples/query/$1.yml"
+    When call query_command_in_file "$2" "samples/valid/$1.yml"
     The status should be success
     The output should eq "$(printf "$3")"
   End
@@ -82,7 +88,7 @@ Describe 'query_command_in_file returns blank when alias not found'
   
   local alias='bee'
   It "getting '$alias' from $1"
-    When call query_command_in_file "$alias" "samples/query/$1.yml"
+    When call query_command_in_file "$alias" "samples/valid/$1.yml"
     The status should be success
     The output should be blank
   End
