b79a84223f2dc77b21086070542643fe5390f5ba
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-14 17:21:45 +0200

    add additional test for Format Error

--- spec/query/query_command_in_file_spec.sh
+++ spec/query/query_command_in_file_spec.sh
@@ -27,24 +27,30 @@ Describe 'query_command_in_file catches format error'
     echo "$3 of which are 'commands'."
   }
 
-  occurance_error() {
+  alias_not_unique_error() {
     echo "Format Error: 'samples/query/[commands][foo,foo].yml' has multiple commands with the alias 'foo'."
     echo "It should have only one."
     echo "It currently has 2 occurances."
   }
 
+  alias_has_no_command() {
+    echo "Format Error: 'samples/query/[commands][has no command].yml' has an alias 'has no command', but no command."
+    echo "Each alias should have exactly one command."
+  }
+
   Parameters
-    '[]' "$(top_level_error "[]" 0 0)"
-    '[notcommands][]' "$(top_level_error "[notcommands][]" 1 0)"
-    '[commands,commands][]' "$(top_level_error "[commands,commands][]" 2 2)"
-    '[commands,notcommands][]' "$(top_level_error "[commands,notcommands][]" 2 1)"
-    '[commands][foo,foo]' "$(occurance_error)"
+    '[]' "foo" "$(top_level_error "[]" 0 0)"
+    '[notcommands][]' "foo" "$(top_level_error "[notcommands][]" 1 0)"
+    '[commands,commands][]' "foo" "$(top_level_error "[commands,commands][]" 2 2)"
+    '[commands,notcommands][]' "foo" "$(top_level_error "[commands,notcommands][]" 2 1)"
+    '[commands][foo,foo]' "foo" "$(alias_not_unique_error)"
+    '[commands][has no command]' "has no command" "$(alias_has_no_command)" 
   End
 
-  It "getting 'foo' from $1"
-    When call query_command_in_file 'foo' "samples/query/$1.yml" 
+  It "getting '$2' from $1"
+    When call query_command_in_file "$2" "samples/query/$1.yml" 
     The status should be failure
-    The stderr should eq "$2"
+    The stderr should eq "$3"
     The stdout should be blank
   End
 End
