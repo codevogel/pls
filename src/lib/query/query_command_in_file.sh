f7bf5bf050a563323c12a4488f6a1ee19429c5de
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-14 15:35:05 +0200

    add validation and fix queries

--- src/lib/query/query_command_in_file.sh
+++ src/lib/query/query_command_in_file.sh
@@ -2,18 +2,36 @@ query_command_in_file() {
   local alias="$1"
   local file="$2"
 
-  local toplevel_nodes_count=$(yq '.[] | length' "$file")
-  local commands_nodes_count=$(yq '.commands | length' "$file")
-  if [[ "$toplevel_nodes_count" -gt 1 || "$commands_nodes_count" -ne 1 ]]; then
-    echo "Error: '$file' should have 'commands' as the top-level node." >&2
-    echo "It currently has $toplevel_nodes_count top-level nodes." >&2
+  if [ -z "$alias" ]; then
+    echo "Usage Error: Alias cannot be empty." >&2
     return 1
   fi
 
-  local num_occurrances="$(yq '.commands | map(select(.alias == "'$alias'")) | length' "$file")"
+  if [ ! -f "$file" ]; then
+    echo "Usage Error: '$file' does not exist." >&2
+    return 1
+  fi
+
+
+  local toplevel_nodes_count="$(yq 'length' "$file")"
+  local commands_nodes_count
+  if [[ "$(yq '. == null' "$file")" == "true" ]]; then
+    commands_nodes_count=0
+  else
+    commands_nodes_count="$(yq 'keys | map(select(. == "commands")) | length' "$file")"
+  fi
+
+  if [[ "$toplevel_nodes_count" -gt 1 || "$commands_nodes_count" -ne 1 ]]; then
+    echo "Format Error: '$file' should have 'commands' as the (only) top-level node." >&2
+    echo "It currently has $toplevel_nodes_count top-level nodes." >&2
+    echo "$commands_nodes_count of which are 'commands'." >&2
+    return 1
+  fi
+
+  local num_occurrances="$(yq ".commands | map(select(.alias == \"$alias\")) | length" "$file")"
 
   if [[ "$num_occurrances" -gt 1 ]]; then
-    echo "Error: '$file' has multiple commands with the alias '$alias'." >&2
+    echo "Format Error: '$file' has multiple commands with the alias '$alias'." >&2
     echo "It should have only one." >&2
     echo "It currently has $num_occurrances occurances." >&2
     return 1
