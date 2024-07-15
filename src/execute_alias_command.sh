91f0933f85bf9093dd97bed14ee6d8b8ec922f81
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-15 22:05:32 +0200

    use query_command

--- src/execute_alias_command.sh
+++ src/execute_alias_command.sh
@@ -1,19 +1,6 @@
 alias="${args[alias]}"
 
-global_file="$PLS_GLOBAL"
-local_file="$(get_closest_file "$PWD" "$PLS_FILE_NAME")"
-
-# Query to get command from yml
-query=".commands[] | select(.alias == \"$alias\") | .command"
-
-# Query the global and local files. 
-# Order is important, as local file has higher precedence
-if [ -f "$global_file" ]; then
-  command="$(yq "$query" "$global_file")"
-fi
-if [ -f "$local_file" ]; then
-  command="$(yq "$query" "$local_file")"
-fi
+command=$(query_command "$alias")
 
 # If no command found, exit
 if [ -z "$command" ]; then
