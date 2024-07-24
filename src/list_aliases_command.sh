e95a80970de2cb521d1b66914b488c60f45e9536
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-24 22:08:15 +0200

    fix list_alias command

--- src/list_aliases_command.sh
+++ src/list_aliases_command.sh
@@ -6,51 +6,56 @@ flag_all="${args[--all]}"
 local_file="$(get_closest_file "$PWD" "$PLS_FILE_NAME")"
 global_file="$PLS_GLOBAL"
 
-
 combine_files() {
+  echo "commands:" > "$PLS_DIR/.combined.yml"
   if [ -f "$local_file" ] && [ -f "$global_file" ]; then
     # Append the two files, replacing commands: with a: and b: respectively
     local temp_file="$(mktemp)"
-    cat "$global_file" | sed "s/commands:/a:/" | sed -E 's/(alias: .*)$/\1/' > "$temp_file"
-    cat "$local_file" | sed "s/commands:/b:/" >> "$temp_file"
-
-    # Create a combined file which will hold the merged commands
-    local combined_file="$PLS_DIR/.combined.yml"
-    echo "commands:" > "$combined_file"
-
-    # Merge the a and b nodes from the temp file into the combined file
-    yq '.a *d .b' "$temp_file" >> "$combined_file"
-    # Cleanup
-    rm "$temp_file"
-  elif [-f "$local_file"]; then
+    sed "s/commands:/local:/" "$local_file" > "$temp_file"
+    sed "s/commands:/global:/" "$global_file" >> "$temp_file"
+    # Add a (*) to all aliases that are in the a: section
+    yq e -i '.commands = ((.global | map(.alias = .alias + " *")) + .local) | del(.global,.local)' "$temp_file"
+    yq eval '
+      .commands |= (
+      map(select(.alias | test(" \\*$") | not)) + 
+      map(select(.alias | test(" \\*$"))) | 
+      unique_by(.alias | sub(" \\*$"; "")) |
+      sort_by(.alias)
+    )' "$temp_file" > "$PLS_DIR/.combined.yml"
+  elif [ -f "$local_file" ]; then
     cat "$local_file" > "$PLS_DIR/.combined.yml"
-  else
-    cat "$global_file" > "$PLS_DIR/.combined.yml"
+  elif [ -f "$global_file" ]; then
+    if [[ "$(yq e '.commands | length' "$global_file")" -eq 0 ]]; then
+      return
+    fi
+    yq eval '.commands | map(.alias = .alias + " *")' "$global_file" >> "$PLS_DIR/.combined.yml"
   fi
 }
 
 list_aliases() {
   local file="$1"
-  local context="$2" # if 'hidden' print no context
-  local should_print_command="$3"
-  if [[ "$context" != "hidden" ]]; then
-    echo "$context"
+  local should_print_command="$2"
+
+  if [[ ! -f "$file" || "$(yq e '.commands | length' "$file")" -eq 0 ]]; then
+    echo "None found."
+    return
   fi
 
-  local alias_query='.commands[] | "-> " + .alias'
-  local command_query='.commands[] | "-> " + .alias + "\n" + (.command | split("\n") | map(select(. != "") | "   " + .) | join("\n"))'
+  local alias_query='.commands | sort_by(.alias) | .[] | "-> " + .alias'
+  local command_query='.commands | sort_by(.alias) | .[] | "-> " + .alias + "\n" + (.command | split("\n") | map(select(. != "") | "   " + .) | join("\n"))'
   yq e "$([[ "$should_print_command" ]] && echo "$command_query" || echo "$alias_query")" "$file"
 }
 
 if [[ "$flag_all" ]]; then
-  list_aliases "$global_file" "Global context ($global_file):" "$flag_command"
-  list_aliases "$local_file" "Local context ($local_file):" "$flag_command"
+  echo "Global aliases:"
+  list_aliases "$global_file" "$flag_command"
+  echo "Local aliases:"
+  list_aliases "$local_file" "$flag_command"
 elif [[ "$flag_global" ]]; then
-  list_aliases "$global_file" "Global context ($global_file):" "$flag_command"
-  echo "Note that these may be overridden by local aliases."
+  list_aliases "$global_file" "$flag_command"
 elif [[ "$flag_local" ]]; then
-  list_aliases "$local_file" "Local context ($local_file):" "$flag_command"
+  list_aliases "$local_file" "$flag_command"
 else
   combine_files
-  list_aliases "$PLS_DIR/.combined.yml" "hidden" "$flag_command"
+  list_aliases "$PLS_DIR/.combined.yml" "$flag_command"
 fi
