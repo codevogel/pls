bb61489d8a69f822fe10b2222aa5a5dbdaedb0ed
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-25 16:05:07 +0200

    add pick_aliases_command

---
+++ src/pick_aliases_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/pick_aliases_command.sh'"
+echo "# code for 'pls pick_aliases' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
