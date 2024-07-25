c7725aabaa5d7315ce22d0a96bfe8498ed84ca51
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-25 16:45:06 +0200

    add pick_alias functionality

--- src/pick_aliases_command.sh
+++ src/pick_aliases_command.sh
@@ -1,4 +1,26 @@
-echo "# this file is located in 'src/pick_aliases_command.sh'"
-echo "# code for 'pls pick_aliases' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+current_list="$(list_aliases)"
+
+if [[ "$current_list" == "No aliases found." ]]; then
+  echo "No aliases found."
+  exit 0
+fi
+
+# if fzf is not installed, exit
+if command -v fzf &> /dev/null; then
+  picked_alias="$(echo "$current_list" | fzf)"
+  # Remove ' *' from the end of the alias if it exists
+else
+  # print the list with
+  # 1 | content
+  echo "$current_list" | awk '{print NR " | " $0}'
+  read -p "Enter alias (or n): " input
+  if [[ "$input" =~ ^[0-9]+$ && "$input" -gt 0 ]]; then
+    # Get alias from line number
+    picked_alias="$(echo "$current_list" | sed -n "${input}p")"
+  else
+    # actually entered an alias instead of a number 
+    picked_alias="$input"
+  fi
+fi
+picked_alias="${picked_alias% \*}"
+execute_alias "$picked_alias"
