264e8cccf3bfacd3f4a0d194c492e27172e9bb50
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-26 14:43:48 +0200

    refactor validation

--- src/lib/validations/validate_is_destination.sh
+++ src/lib/validations/validate_is_destination.sh
@@ -1,8 +1,4 @@
 validate_is_destination() {
   local arg="$1"
-  if [ "$arg" != "g" ] && [ "$arg" != "l" ] && [ "$arg" != "h" ] && [ "$arg" != "global" ] && [ "$arg" != "local" ] && [ "$arg" != "here" ]; then
-    echo "Error: Invalid argument. Must be one of [ g, global, l, local, h, here ]"
-    return 1
-  fi
-  
+  [ "$arg" != "g" ] && [ "$arg" != "l" ] && [ "$arg" != "h" ] && [ "$arg" != "global" ] && [ "$arg" != "local" ] && [ "$arg" != "here" ] && echo "Error: Invalid argument. Must be one of [ g, global, l, local, h, here ]"
 }
