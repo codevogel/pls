f551fbf41e885ed87cc1db1d3fc81bf949ea514a
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-26 12:58:53 +0200

    add add_alias_command

---
+++ src/add_alias_command.sh
@@ -0,0 +1,27 @@
+alias="${args[--alias]}"
+command="${args[--command]}"
+destination="${args[--destination]}"
+
+target_file="$(destination_to_path "$destination")"
+
+# Create file if it doesn't exist
+if [ ! -f "$target_file" ]; then
+  echo "commands:" > "$target_file"
+fi
+
+# If alias already exists in file, --force flag must be used
+num_occurances_of_alias="$(yq e ".commands | map(select(.alias == \"$alias\")) | length" "$target_file")"
+if [[ $num_occurances_of_alias -gt 1 ]]; then
+  echo "Format Error: Multiple occurrances of alias \"$alias\" found in file \"$target_file\"." >&2
+  return 1
+elif [[ $num_occurances_of_alias -eq 1 ]]; then
+  if [ ! "${args[--force]}" ]; then
+    echo "Error: Alias \"$alias\" already exists in file \"$target_file\". Use --force to overwrite." >&2
+    return 1
+  fi
+  # Remove the old alias
+  delete_alias_from_file "$alias" "$target_file"
+fi
+
+# Add alias:command entry to file
+add_entry_to_file "$alias" "$command" "$target_file"
