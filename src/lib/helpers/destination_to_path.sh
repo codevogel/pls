7908a97884c3bdeb1c29252240cf159f1d0fefbd
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-26 16:42:16 +0200

    return blank if local file does not return anything

--- src/lib/helpers/destination_to_path.sh
+++ src/lib/helpers/destination_to_path.sh
@@ -7,16 +7,13 @@ destination_to_path() {
   # If the destination is "l" or "local", return the closest file
   elif [ "$destination" == "l" ] || [ "$destination" == "local" ]; then
     local closest_file="$(get_closest_file "$PWD" "$PLS_FILENAME")"
-    # If a closest file is found, return it
-    if [ -n "$closest_file" ]; then
-      echo "$closest_file"
-      return 0
-    fi
-  # If the destination is not "h" or "here", then it must be invalid
-  elif [ "$destination" != "h" ] && [ "$destination" != "here" ]; then
-    echo "Error: Invalid argument. Must be one of [ g, global, l, local, h, here ]" >&2
-    return 1
-  fi
+    echo "$closest_file"
+    return 0
   # Return destination in the current directory
-  echo "$(realpath $PWD/$PLS_FILENAME)"
+  elif [ "$destination" == "h" ] || [ "$destination" == "here" ]; then
+    echo "$(realpath $PWD/$PLS_FILENAME)"
+    return 0
+  fi
+  echo "Error: Invalid argument. Must be one of [ g, global, l, local, h, here ]" >&2
+  return 1
 }
