aa17abf66cb076ff32929ddcef3a3aac6ece7d17
Kamiel de Visser <kamieldevisser@gmail.com>
2024-08-05 22:35:46 +0200

    move add_alias_command into root and use flags

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,4 +1,12 @@
 alias="${args[alias]}"
+flag_add="${args[--add]}"
+flag_command="${args[--command]}"
+flag_destination="${args[--destination]}"
+
+if [ -n "$flag_add" ]; then
+  add_alias
+  exit
+fi
 
 if [ -z "$alias" ]; then
   pick_and_execute_alias
